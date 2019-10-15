c7c8b52cda4e8dd1cb0c42379e1ce8bce29d34a5
Pavel Buldakov <pavel_buldakov@epam.com>
2019-10-15 10:09:00 +0400

    3 task complite

--- src/app/components/course-list/course-list.component.spec.ts
+++ src/app/components/course-list/course-list.component.spec.ts
@@ -2,48 +2,31 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CourseListComponent } from './course-list.component';
 import { FormsModule } from '@angular/forms';
-import { CourseItemComponent } from './course-item/course-item.component';
 import { Course } from 'src/app/models/common-module';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('CourseListComponent', () => {
   let component: CourseListComponent;
   let fixture: ComponentFixture<CourseListComponent>;
-  const courses: Course[] = [
-    {
-    id: "1",
-    title: "title",
-    description: "description",
-    duration: new Date(),
-    creationDate: new Date(),
-    }
-  ];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CourseListComponent, CourseItemComponent ],
-      imports: [FormsModule]
+      declarations: [ CourseListComponent ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents().then(() => {
       fixture = TestBed.createComponent(CourseListComponent);
       component = fixture.componentInstance;
-      component.courseList = courses;
     });
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CourseListComponent);
     component = fixture.componentInstance;
-    component.courseList = courses;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-
-  it('should be emit course id', () => {
-    const buttonDelete: HTMLElement = fixture.debugElement.nativeElement.querySelectorAll('button')[1];
-    buttonDelete.click();
-    expect(component.courseIdDeleted).toBe('1');
-  })
 });
--- src/app/components/pages/courses/courses.component.spec.ts
+++ src/app/components/pages/courses/courses.component.spec.ts
@@ -1,10 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CoursesComponent } from './courses.component';
-import { CourseListComponent } from '../../course-list/course-list.component';
-import { CourseItemComponent } from '../../course-list/course-item/course-item.component';
-import { SearchComponent } from '../../search/search.component';
 import { FormsModule } from '@angular/forms';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('CoursesComponent', () => {
   let component: CoursesComponent;
@@ -12,8 +10,9 @@ describe('CoursesComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CoursesComponent, CourseListComponent, CourseItemComponent, SearchComponent ],
-      imports: [FormsModule]
+      declarations: [ CoursesComponent ],
+      imports: [FormsModule],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents();
   }));
