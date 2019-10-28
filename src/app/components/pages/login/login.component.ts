82ce20a4a7f6ad87fe15fcca596e7195d6a948a4
Pavel Buldakov <pavel_buldakov@epam.com>
2019-10-28 17:38:43 +0400

    wip

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login/login.service';
+import { User } from 'src/app/models/common-module';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  private _email: string;
+  get email(): string {
+    return this._email;
+  }
+  set email(value: string) {
+    this._email = value;
+  }
 
-  constructor() { }
+  private _password: string;
+  get password(): string {
+    return this._password;
+  }
+
+  set password(value: string) {
+    this._password = value;
+  }
+
+  constructor(private loginService: LoginService) { }
 
   ngOnInit() {
+  }
+
+  public onSubmit() {
+    const newUser = {
+      id: '',
+      firstName: '',
+      LastName: '',
+      password: this.password,
+      email: this.email,
+    };
+    this.loginService.login(new User(newUser));
   }
 
 }
